38aa70f8088032959be40e27d44f9b6d480cce9e
Vandivier <john@ladderly.io>
2025-01-05 00:22:37 -0500

    feat: favicon and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,21 @@ const GA_MEASUREMENT_ID = 'G-8QGWK8VL4Q';
 export const metadata: Metadata = {
   title: 'AfterEcon',
   description: "John Vandivier's blog on economics and more",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
